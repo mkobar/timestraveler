afc777c3a3ba564e6b9d04edf49f27ef82ca3437
mkobar <mike@exlorev.com>
2015-04-27 20:32:57 -0400

    removed filter code

--- js/timestraveler.js
+++ js/timestraveler.js
@@ -44,20 +44,6 @@ $.each(json.results, function(index, element) {
    L.marker([element.geocode.latitude, element.geocode.longitude]).addTo(map);
 });
     current_results = json.results;
-
-    /**
-
-
-    // now load initial element[pic_index].url to vote page
-    $('#vote_pic').attr("src",json.pics[pic_index].url);
-
-    // now load data on restaurant page
-    $('#rest_sv').attr("src",json.streetview);
-    $('#rest_name').html("<h2>" + json.name + "</h2>");
-    $('#rest_address').html("<b>" + json.address + "</b>");
-    $('#rest_phone').html("<b>" + json.phone + "</b>");
-    $('#rest_web').html("<a href=\"json.website\" target=_blank>" + json.website + "</a>");
-    **/
 	}
 
     // including the google map part
@@ -89,10 +75,6 @@ var track_id = '';      // Name/ID of the exercise
 var watch_id = null;    // ID of the geolocation
 var tracking_data = []; // Array containing GPS position objects
 
-var rest_status =0;
-var best_count =0;   // count of itmes in best5_data
-var pic_index =0;  // where we are in the current pic array
-var pic_page =0;   // what page we have loaded with GET
 var current_pics = [];  // array of pics from GET
 var current_results = [];  // array of pics from GET
 
@@ -101,27 +83,6 @@ var current_results = [];  // array of pics from GET
 $("#map-vote").on('click', function(){
 	rest_status = get_gtag();
 	location.href = "#mapview";
-});
-
-$.mockjax({
-  url: '/restful/restaurant/4',
-  responseTime: 750,
-  responseText: {
-    status: 'success',
-    name: 'Sallys Apizza',
-    picid: 004,
-    restid: 4,
-    lat: 41.27,
-    lon: -72.25,
-    url: 'sallys-apizza.jpg',
-    phone: '(203) 624-5271',
-    address: '237 Wooster St New Haven, CT 06511',
-    website: 'http://www.sallysapizza.com/',
-    streetview: 'sv-sallys-apizza.jpg',
-    reservation: null,
-    delivery: null,
-    likes: 5
-  }
 });
 
 });
